38f32d406f38a56280bf4aa105c3ac79eceb9dd6
joaoppagnan <joaopedroopagnan@gmail.com>
2022-06-26 21:18:30 -0300

    Added some more layers

---
+++ src/models/convlayer.py
@@ -0,0 +1,57 @@
+import numpy as np
+import torch
+
+class MyConv2d(torch.nn.Module):
+    """Conv layer with equalized learning rate and custom learning rate multiplier."""
+    def __init__(self, input_channels, output_channels, kernel_size, gain=2**(0.5), use_wscale=False, lrmul=1, bias=True,
+                intermediate=None, upscale=False):
+        super().__init__()
+        if upscale:
+            self.upscale = Upscale2d()
+        else:
+            self.upscale = None
+        he_std = gain * (input_channels * kernel_size ** 2) ** (-0.5) # He init
+        self.kernel_size = kernel_size
+        if use_wscale:
+            init_std = 1.0 / lrmul
+            self.w_mul = he_std * lrmul
+        else:
+            init_std = he_std / lrmul
+            self.w_mul = lrmul
+        self.weight = torch.nn.Parameter(torch.randn(output_channels, input_channels, kernel_size, kernel_size) * init_std)
+        if bias:
+            self.bias = torch.nn.Parameter(torch.zeros(output_channels))
+            self.b_mul = lrmul
+        else:
+            self.bias = None
+        self.intermediate = intermediate
+
+    def forward(self, x):
+        bias = self.bias
+        if bias is not None:
+            bias = bias * self.b_mul
+        
+        have_convolution = False
+        if self.upscale is not None and min(x.shape[2:]) * 2 >= 128:
+            # this is the fused upscale + conv from StyleGAN, sadly this seems incompatible with the non-fused way
+            # this really needs to be cleaned up and go into the conv...
+            w = self.weight * self.w_mul
+            w = w.permute(1, 0, 2, 3)
+            # probably applying a conv on w would be more efficient. also this quadruples the weight (average)?!
+            w = F.pad(w, (1,1,1,1))
+            w = w[:, :, 1:, 1:]+ w[:, :, :-1, 1:] + w[:, :, 1:, :-1] + w[:, :, :-1, :-1]
+            x = F.conv_transpose2d(x, w, stride=2, padding=(w.size(-1)-1)//2)
+            have_convolution = True
+        elif self.upscale is not None:
+            x = self.upscale(x)
+    
+        if not have_convolution and self.intermediate is None:
+            return torch.nn.functional.conv2d(x, self.weight * self.w_mul, bias, padding=self.kernel_size//2)
+        elif not have_convolution:
+            x = torch.nn.functional.conv2d(x, self.weight * self.w_mul, None, padding=self.kernel_size//2)
+        
+        if self.intermediate is not None:
+            x = self.intermediate(x)
+        if bias is not None:
+            x = x + bias.view(1, -1, 1, 1)
+        return x
---
+++ src/models/upscalelayer.py
@@ -0,0 +1,22 @@
+import numpy as np
+import torch
+
+class Upscale2D(torch.nn.Module):
+    def __init__(self, factor=2, gain=1):
+        super().__init__()
+        assert isinstance(factor, int) and factor >= 1
+        self.gain = gain
+        self.factor = factor
+
+    def _upscale2d(x, factor=2, gain=1):
+        assert x.dim() == 4
+        if gain != 1:
+            x = x * gain
+        if factor != 1:
+            shape = x.shape
+            x = x.view(shape[0], shape[1], shape[2], 1, shape[3], 1).expand(-1, -1, -1, factor, -1, factor)
+            x = x.contiguous().view(shape[0], shape[1], factor * shape[2], factor * shape[3])
+        return x
+
+    def forward(self, x):
+        return self._upscale2d(x, factor=self.factor, gain=self.gain)
