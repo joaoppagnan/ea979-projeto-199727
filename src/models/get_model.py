bfcac45d5ff34c0618a5be79a8108b926167113c
joaoppagnan <joaopedroopagnan@gmail.com>
2022-07-02 16:47:40 -0300

    Updated the code to download the pretrained model

--- src/models/get_model.py
+++ src/models/get_model.py
@@ -1,8 +1,9 @@
 import os
-from src.utils import download_url, unzip_file
+from src.utils.download_url import download_url
+from src.utils.unzip_file import unzip_file
 
 def get_model(path:str, fname:str, url:str):
     os.makedirs(path, exist_ok=True)
-    download_url(url, path, fname=fname)
+    download_url(url, fname=fname)
     unzip_file(filename=fname, destination=path)
     os.unlink(fname)
