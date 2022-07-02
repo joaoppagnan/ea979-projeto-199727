cc6628c3eddaf1691bd807adb0af4a3368625a84
joaoppagnan <joaopedroopagnan@gmail.com>
2022-07-02 13:59:54 -0300

    Updated some things in the function to download the data files

--- src/data/get_data.py
+++ src/data/get_data.py
@@ -3,5 +3,6 @@ from src.utils import download_url, unzip_file
 
 def get_data(path:str, fname:str, url:str):
     os.makedirs(path, exist_ok=True)
-    download_url(path, fname=fname)
+    download_url(url, path, fname=fname)
     unzip_file(filename=fname, destination=path)
+    os.unlink(fname)
